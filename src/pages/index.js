747b81946e8522edbaaa41cd042074b28f8342f2
spanayi02 <spanay02@ucy.ac.cy>
2024-07-08 15:57:05 +0300

    Starting the website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,36 @@
-export const getServerSideProps = async () => {
-    const res = await fetch('http://localhost:3000/api/products');
-    const ProductsAnthia = await res.json();
-    return {
-      props: { ProductsAnthia: Array.isArray(ProductsAnthia) ? ProductsAnthia : [] },
-    };
-   };
-   export default function ProductsAnthia({ ProductsAnthia }) {
-    return (
-   <div>
-        {ProductsAnthia.map((p) => {
-          return (
-   <div key={p.ProductID}>
-   <p>
-                {p.Name} - {p.RetailPrice}
-   </p>
-   </div>
-          );
-        })}
-   </div>
-    );
-   }
+// export const getServerSideProps = async () => {
+//     const res = await fetch('http://localhost:3000/api/products');
+//     const ProductsAnthia = await res.json();
+//     return {
+//       props: { ProductsAnthia: Array.isArray(ProductsAnthia) ? ProductsAnthia : [] },
+//     };
+//    };
+//    export default function ProductsAnthia({ ProductsAnthia }) {
+//     return (
+//    <div>
+//         {ProductsAnthia.map((p) => {
+//           return (
+//    <div key={p.ProductID}>
+//    <p>
+//                 {p.Name} - {p.RetailPrice}
+//    </p>
+//    </div>
+//           );
+//         })}
+//    </div>
+//     );
+//    }
+
+// pages/index.js
+import Layout from '../components/layout.js';
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1>Home Page</h1>
+      <p>Welcome to our Next.js application.</p>
+    </Layout>
+  );
+};
+
+export default Home;
