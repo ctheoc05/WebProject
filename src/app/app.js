04a05f4abd5096c858700ec3219e2f7b3c3337ad
ctheoc05 <ctheoc05@ucy.ac.cy>
2024-07-09 12:20:26 +0300

    Header

---
+++ src/app/app.js
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+import "./app.css"
+
+function app(){
+    return <div className="app">
+        {/* Header Section */}
+        <Header/>
+
+        {/*Main Container*/}
+        {/*Bottom Menu*/}
+
+
+    </div>
+}
+
+export default app;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react"
+import {} from "@mui/material"
+import {SearchRounded, ShoppingCartRounded} from "@mui/icons-material"
+
+function Header() {
+  return (
+    <header>
+      <img src="" alt="" />
+      <div className="inputBox">
+        <SearchRounded className="searchIcon"/>
+        <input type="text" placeholder="Search"/>
+      </div>
+
+      <div className="shoppingCart">
+        <SearchCartRounded className="cart"/>
+        <div className="cart_content"/>
+        <p>3</p>
+      </div>
+
+
+    </header>
+  )
+}
+
+export default Header
