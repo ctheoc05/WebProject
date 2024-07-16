11411e44c02493edc5f7693bb2f4e0709fd9f70d
ctheoc05 <ctheoc05@ucy.ac.cy>
2024-07-16 14:56:17 +0300

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,44 @@
-import PageContainer from '../components/PageContainer'
-import Main from "../components/Main";
-import Title from "../components/Title";
-import Description from "../components/Description";
+import React from 'react';
+import Image from 'next/image';
+import styles from './about.module.css';
 
-export default  function About() {
+const About = () => {
   return (
-    <PageContainer>
-      <Main>
-        <Title>
-          About us:
-        </Title>
-        <Description>
-          some info about us..
-        </Description>
-        Contact us: 99344588
-      </Main>
-    </PageContainer>
-  )
-}
+    <div className={styles.container}>
+      <Image
+        src="/lalala.png"
+        alt="Flower Shop Logo"
+        width={150}
+        height={150}
+        className={styles.logo}
+      />
+      <h1 className={styles.title}>About Our Flower Shop</h1>
+      <p className={styles.description}>
+        Welcome to our enchanting flower shop, where we specialize in crafting
+        the most exquisite floral arrangements for every occasion.
+      </p>
+      <p className={styles.description}>
+        At our shop, we believe in the magic of flowers to brighten any day and
+        add a touch of elegance to every celebration. Whether you're looking
+        for a stunning bouquet for a loved one or floral decorations for an
+        event, we are here to fulfill your floral fantasies.
+      </p>
+      <p className={styles.description}>
+        Our team of skilled florists carefully selects each bloom to ensure
+        every arrangement is a work of art. With a passion for creativity and
+        an eye for detail, we create designs that captivate and inspire.
+      </p>
+      <p className={styles.description}>
+        Browse through our collection and let our flowers speak to you. We
+        look forward to helping you express your emotions through the language
+        of flowers.
+      </p>
+    </div>
+  );
+};
+
+
+
+
+export default About;
+
