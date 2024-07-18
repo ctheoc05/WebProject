d02c9fa6673e9153d82fdb1a1d499dada4786117
ctheoc05 <ctheoc05@ucy.ac.cy>
2024-07-18 12:57:10 +0300

    db pull

---
+++ src/components/Navbar.js
@@ -0,0 +1,88 @@
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { ShoppingCartRounded } from "@mui/icons-material";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const [scroll, setScroll] = useState(false);
+
+  const links = [
+    { id: 1, link: "Home", href: "/" },
+    { id: 2, link: "About", href: "/about" },
+    { id: 3, link: "Contact", href: "/contact" },
+    { id: 4, link: "Account", href: "/api/signup" },
+  ];
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScroll(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`flex justify-between items-center w-full h-20 px-4 text-black bg-white fixed top-0 z-50 transition-all duration-300 ${
+        scroll ? "shadow-lg bg-opacity-90" : "bg-opacity-100"
+      }`}
+    >
+      <div>
+        <h1 className="text-5xl font-signature ml-2">
+          <a
+            className="link-underline link-underline-black"
+            href=""
+            target="_blank"
+            rel="noreferrer"
+          >
+            Logo
+          </a>
+        </h1>
+      </div>
+
+      <ul className="hidden md:flex">
+        {links.map(({ id, link, href }) => (
+          <li
+            key={id}
+            className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-black duration-200 link-underline"
+          >
+            <Link href={href}>{link}</Link>
+          </li>
+        ))}
+        <li className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-black duration-200 link-underline">
+          <Link href="/cart">
+            <ShoppingCartRounded className="cart" />
+          </Link>
+        </li>
+      </ul>
+
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link, href }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              <Link onClick={() => setNav(!nav)} href={href}>
+                {link}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
